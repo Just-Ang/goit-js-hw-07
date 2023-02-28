d8904719700e67949aea6024d928ff77ae714e49
Angelica <lisoveckaya1997@gmail.com>
2023-02-28 18:47:42 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const items = galleryItems
+  .map(
+    (item) => `<div class='gallery__item'>
+ <a class='gallery__link' href="${item.original}"><img class="gallery__image" 
+ src = "${item.preview}" 
+ data-source ="${item.original}" 
+ alt="${item.description}"
+ </a>
+  </div>`
+  )
+  .join("");
+
+console.log(items);
+
+galleryEl.innerHTML = items;
