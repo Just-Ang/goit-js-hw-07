f093a7c1e657596addc8226b9af1b190f7f7f930
Angelica <lisoveckaya1997@gmail.com>
2023-02-28 20:30:48 +0200

    changed 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,7 +6,7 @@ const galleryEl = document.querySelector(".gallery");
 const items = galleryItems
   .map(
     (item) => `<div class="gallery__item">
-    <a class="gallery__link" href="${item.original}">
+    <a class="gallery__link" href="${item.original}" onclick="event.preventDefault()">
       <img
         class="gallery__image"
         src="${item.preview}"
@@ -18,15 +18,30 @@ const items = galleryItems
   )
   .join("");
 
-
 galleryEl.innerHTML = items;
-
-
 
 galleryEl.addEventListener('click', selectImg);
 
 function selectImg(event) {
-    if (event.target.className === "gallery__image") {
-        console.log('Click!');
+    if (event.target.nodeName !== "IMG") {
+        return;
     }  
-}
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+instance.show()
+
+galleryEl.addEventListener('keydown', closeEsc);
+function closeEsc(event) {
+    if (event.code === "Escape") {
+        instance.close();
+    }
+}}
+
+
+
+
+
+
+
+
