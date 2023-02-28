237df191b51b49d52a72fab2df8d29904145501c
Angelica <lisoveckaya1997@gmail.com>
2023-02-28 21:01:31 +0200

    changed 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const items = galleryItems
+  .map(
+    (item) => `<a class="gallery__item" href="${item.original}" onclick="event.preventDefault()">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>`
+  )
+  .join("");
+
+galleryEl.innerHTML = items;
+
+galleryEl.addEventListener('click', selectImg);
+
+function selectImg(event) {
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }  
+    var lightbox = new SimpleLightbox(".gallery a", {
+        captionSelector: 'img',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+        scrollZoom: false,
+       });
+    
+
+}
+
